44918226e6a93c30bd648b10a9b23655a6dd2137
Mamin78 <aminsh78@gmail.com>
2021-05-12 17:06:25 +0430

    The project works well

--- Main.go
+++ Main.go
@@ -15,6 +15,7 @@ var badMethodErr = "Bad method error!"
 var badTaskErr = "Bad task error!"
 var extraFieldErr = "Extra field error!"
 var typeErr = "Type mismatch!"
+var incorrectErr = "Your input is incorrect"
 
 type Request struct {
 	Task interface{}            `json:"task"`
@@ -152,8 +153,6 @@ func unmarshalRequest(w http.ResponseWriter, reqBody string) *SliceRequest {
 	var request *SliceRequest
 	err := json.Unmarshal([]byte(reqBody), &request)
 	if err != nil {
-		fmt.Fprintln(w, "iner")
-
 		notSlice := unmarshalNotSliceRequest(w, reqBody)
 		if notSlice == nil {
 			return nil
@@ -182,7 +181,7 @@ func unmarshalNotSliceRequest(w http.ResponseWriter, reqBody string) *NotSliceRe
 	var request *NotSliceRequest
 	err := json.Unmarshal([]byte(reqBody), &request)
 	if err != nil {
-		fmt.Fprintln(w, typeErr)
+		fmt.Fprintln(w, incorrectErr)
 		return nil
 	}
 	return request
@@ -311,37 +310,4 @@ func main() {
 	http.HandleFunc("/history", history)
 
 	http.ListenAndServe(":8080", nil)
-	//
-	//var1 := "hello world"
-	//
-	//// integer
-	//var2 := 10
-	//
-	//// float
-	//var3 := 1.55
-	//
-	//// boolean
-	//var4 := true
-	//
-	//// shorthand string array declaration
-	//var5 := []string{"foo", "bar", "baz"}
-	//
-	//// map is reference datatype
-	//var6 := map[int]string{100: "Ana", 101: "Lisa", 102: "Rob"}
-	//
-	//// complex64 and complex128
-	//// is basic datatype
-	//var7 := complex(9, 15)
-	//
-	//var x []interface{}
-	//x = append(x,2)
-	//
-	//fmt.Println("var1 = ", reflect.ValueOf(var1).Kind() == reflect.Int)
-	//fmt.Println("var2 = ", reflect.ValueOf(var2).Kind())
-	//fmt.Println("var3 = ", reflect.ValueOf(var3).Kind())
-	//fmt.Println("var4 = ", reflect.ValueOf(var4).Kind())
-	//fmt.Println("var5 = ", reflect.ValueOf(var5).Kind())
-	//fmt.Println("var6 = ", reflect.ValueOf(var6).Kind())
-	//fmt.Println("var7 = ", reflect.ValueOf(var7).Kind())
-	//fmt.Println("var7 = ", reflect.ValueOf(x[0]).Kind())
 }
